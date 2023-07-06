74d3467868769fe61310a5e393d7278b8991d262
AhmadiGu <ahmadigulalai76@gmail.com>
2023-07-06 18:34:17 +0500

    The main page desgined and structured

---
+++ src/components/checkIn_all.tsx
@@ -0,0 +1,11 @@
+import CheckIns from "./checkIns";
+import Navbar from "./head";
+
+export default function Check_All() {
+  return (
+    <div>
+      <Navbar />
+      <CheckIns />
+    </div>
+  );
+}
